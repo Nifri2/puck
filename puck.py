94a8d3c8215d9cc53ffebfeddf103cfb844c4a71
Niklas Fritsch <ev-m-nf@Niklass-MBP.fritz.box>
2022-07-27 17:18:11 +0200

    added char encoding

--- puck.py
+++ puck.py
@@ -7,5 +7,20 @@ dec = {
 
 number = lambda x : '+'.join([str(dec['one']) for i in range(x)])
 
-print(dec)
+q = "'"
+chars = {
+    "c": f'str(str)[{number(1)}]',
+    "h": f"str(chr)[-({number(3)})]",
+    "r": f"str(chr)[-({number(2)})]",
+    "o": f'str(ord)[-({number(4)})]',
+    "d": f"str(ord)[-({number(2)})]",
+}
+
+def create_char(char):
+    c = f"{chars['o']}+{chars['r']}+{chars['d']}"
+    return f'chr(eval({q}{number(eval(eval(c)+f"({q}{char}{q})"))}{q}))'
+
+
+print(chars)
+print(dec)  
 print(number(7))
